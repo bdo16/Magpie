b0d37fe2590c89b0bf33d9e7b24bb2ddf8002ae9
bdo16 <bdo16@punahou.edu>
2014-11-09 14:47:56 -1000

    Activity 5
    Used arrays to make keeping track of default responses easier.

--- Magpie.java
+++ Magpie.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * A program to carry on conversations with a human user.
  * This is the initial version that:  
@@ -96,212 +98,190 @@ public class Magpie
       response = transformYouMeStatement(statement);
     }
     
+    //else returns default response
     else
     {
       response = getRandomResponse();
     }
-  }
-  return response;
-}
-
-/**
- * Take a statement with "I want to <something>." and transform it into 
- * "What would it mean to <something>?"
- * @param statement the user statement, assumed to contain "I want to"
- * @return the transformed statement
- */
-private String transformIWantToStatement(String statement)
-{
-  //  Remove the final period, if there is one
-  statement = statement.trim();
-  String lastChar = statement.substring(statement.length() - 1);
-  if (lastChar.equals("."))
-  {
-    statement = statement.substring(0, statement.length() - 1);
-  }
-  int psn = findKeyword (statement, "I want to", 0);
-  String restOfStatement = statement.substring(psn + 9).trim();
-  return "What would it mean to " + restOfStatement + "?";
-}
-
-private String transformIWantStatement(String statement)
-{
-  statement = statement.trim();
-  String lastChar = statement.substring(statement.length() - 1);
-  if (lastChar.equals("."))
-  {
-    statement = statement.substring(0, statement.length() - 1);
-  }
-  int psn = findKeyword(statement, "I want", 0);
-  String restOfStatement = statement.substring(psn + 6).trim();
-  return "Would you really be happy if you had " + restOfStatement + "?";
-}
-
-/**
- * Take a statement with "you <something> me" and transform it into 
- * "What makes you think that I <something> you?"
- * @param statement the user statement, assumed to contain "you" followed by "me"
- * @return the transformed statement
- */
-private String transformYouMeStatement(String statement)
-{
-  //  Remove the final period, if there is one
-  statement = statement.trim();
-  String lastChar = statement.substring(statement.length() - 1);
-  if (lastChar.equals("."))
-  {
-    statement = statement.substring(0, statement.length() - 1);
+    return response;
   }
   
-  int psnOfYou = findKeyword (statement, "you", 0);
-  int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
-  
-  String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
-  return "What makes you think that I " + restOfStatement + " you?";
-}
-
-
-private String transformIYouStatement(String statement)
-{
-  //  Remove the final period, if there is one
-  statement = statement.trim();
-  String lastChar = statement.substring(statement.length() - 1);
-  if (lastChar.equals("."))
+  /**
+   * Take a statement with "I want to <something>." and transform it into 
+   * "What would it mean to <something>?"
+   * @param statement the user statement, assumed to contain "I want to"
+   * @return the transformed statement
+   */
+  private String transformIWantToStatement(String statement)
   {
-    statement = statement.substring(0, statement.length() - 1);
-  }
-  
-  int psnOfI = findKeyword (statement, "i", 0);
-  int psnOfYou = findKeyword (statement, "you", psnOfI + 1);
-  
-  String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
-  return "Why do you " + restOfStatement + " me?";
-}
-
-
-/**
- * Search for one word in phrase. The search is not case
- * sensitive. This method will check that the given goal
- * is not a substring of a longer string (so, for
- * example, "I know" does not contain "no").
- * 
- * @param statement
- *            the string to search
- * @param goal
- *            the string to search for
- * @param startPos
- *            the character of the string to begin the
- *            search at
- * @return the index of the first occurrence of goal in
- *         statement or -1 if it's not found
- */
-private int findKeyword(String statement, String goal,
-                        int startPos)
-{
-  String phrase = statement.trim();
-  // The only change to incorporate the startPos is in
-  // the line below
-  int psn = phrase.toLowerCase().indexOf(
-                                         goal.toLowerCase(), startPos);
-  
-  // Refinement--make sure the goal isn't part of a
-  // word
-  while (psn >= 0)
-  {
-    // Find the string of length 1 before and after
-    // the word
-    String before = " ", after = " ";
-    if (psn > 0)
+    //  Remove the final period, if there is one
+    statement = statement.trim();
+    String lastChar = statement.substring(statement.length() - 1);
+    if (lastChar.equals("."))
     {
-      before = phrase.substring(psn - 1, psn)
-        .toLowerCase();
+      statement = statement.substring(0, statement.length() - 1);
     }
-    if (psn + goal.length() < phrase.length())
+    int psn = findKeyword (statement, "I want to", 0);
+    String restOfStatement = statement.substring(psn + 9).trim();
+    return "What would it mean to " + restOfStatement + "?";
+  }
+  
+  private String transformIWantStatement(String statement)
+  {
+    statement = statement.trim();
+    String lastChar = statement.substring(statement.length() - 1);
+    if (lastChar.equals("."))
     {
-      after = phrase.substring(
-                               psn + goal.length(),
-                               psn + goal.length() + 1)
-        .toLowerCase();
+      statement = statement.substring(0, statement.length() - 1);
+    }
+    int psn = findKeyword(statement, "I want", 0);
+    String restOfStatement = statement.substring(psn + 6).trim();
+    return "Would you really be happy if you had " + restOfStatement + "?";
+  }
+  
+  /**
+   * Take a statement with "you <something> me" and transform it into 
+   * "What makes you think that I <something> you?"
+   * @param statement the user statement, assumed to contain "you" followed by "me"
+   * @return the transformed statement
+   */
+  private String transformYouMeStatement(String statement)
+  {
+    //  Remove the final period, if there is one
+    statement = statement.trim();
+    String lastChar = statement.substring(statement.length() - 1);
+    if (lastChar.equals("."))
+    {
+      statement = statement.substring(0, statement.length() - 1);
     }
     
-    // If before and after aren't letters, we've
-    // found the word
-    if (((before.compareTo("a") < 0) || (before
-                                           .compareTo("z") > 0)) // before is not a
-          // letter
-          && ((after.compareTo("a") < 0) || (after
-                                               .compareTo("z") > 0)))
+    int psnOfYou = findKeyword (statement, "you", 0);
+    int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
+    
+    String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
+    return "What makes you think that I " + restOfStatement + " you?";
+  }
+  
+  
+  private String transformIYouStatement(String statement)
+  {
+    //  Remove the final period, if there is one
+    statement = statement.trim();
+    String lastChar = statement.substring(statement.length() - 1);
+    if (lastChar.equals("."))
     {
-      return psn;
+      statement = statement.substring(0, statement.length() - 1);
     }
     
-    // The last position didn't work, so let's find
-    // the next, if there is one.
-    psn = phrase.indexOf(goal.toLowerCase(),
-                         psn + 1);
+    int psnOfI = findKeyword (statement, "i", 0);
+    int psnOfYou = findKeyword (statement, "you", psnOfI + 1);
     
+    String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+    return "Why do you " + restOfStatement + " me?";
   }
   
-  return -1;
-}
-
-/**
- * Search for one word in phrase. The search is not case
- * sensitive. This method will check that the given goal
- * is not a substring of a longer string (so, for
- * example, "I know" does not contain "no"). The search
- * begins at the beginning of the string.
- * 
- * @param statement
- *            the string to search
- * @param goal
- *            the string to search for
- * @return the index of the first occurrence of goal in
- *         statement or -1 if it's not found
- */
-private int findKeyword(String statement, String goal)
-{
-  return findKeyword(statement, goal, 0);
-}
-
-
-
-
-/**
- * Pick a default response to use if nothing else fits.
- * @return a non-committal string
- */
-private String getRandomResponse()
-{
-  final int NUMBER_OF_RESPONSES = 6;
-  double r = Math.random();
-  int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-  String response = "";
   
-  if (whichResponse == 0)
+  /**
+   * Search for one word in phrase. The search is not case
+   * sensitive. This method will check that the given goal
+   * is not a substring of a longer string (so, for
+   * example, "I know" does not contain "no").
+   * 
+   * @param statement
+   *            the string to search
+   * @param goal
+   *            the string to search for
+   * @param startPos
+   *            the character of the string to begin the
+   *            search at
+   * @return the index of the first occurrence of goal in
+   *         statement or -1 if it's not found
+   */
+  private int findKeyword(String statement, String goal,
+                          int startPos)
   {
-    response = "Interesting, tell me more.";
+    String phrase = statement.trim();
+    // The only change to incorporate the startPos is in
+    // the line below
+    int psn = phrase.toLowerCase().indexOf(
+                                           goal.toLowerCase(), startPos);
+    
+    // Refinement--make sure the goal isn't part of a
+    // word
+    while (psn >= 0)
+    {
+      // Find the string of length 1 before and after
+      // the word
+      String before = " ", after = " ";
+      if (psn > 0)
+      {
+        before = phrase.substring(psn - 1, psn)
+          .toLowerCase();
+      }
+      if (psn + goal.length() < phrase.length())
+      {
+        after = phrase.substring(
+                                 psn + goal.length(),
+                                 psn + goal.length() + 1)
+          .toLowerCase();
+      }
+      
+      // If before and after aren't letters, we've
+      // found the word
+      if (((before.compareTo("a") < 0) || (before
+                                             .compareTo("z") > 0)) // before is not a
+            // letter
+            && ((after.compareTo("a") < 0) || (after
+                                                 .compareTo("z") > 0)))
+      {
+        return psn;
+      }
+      
+      // The last position didn't work, so let's find
+      // the next, if there is one.
+      psn = phrase.indexOf(goal.toLowerCase(),
+                           psn + 1);
+      
+    }
+    
+    return -1;
   }
-  else if (whichResponse == 1)
+  
+  /**
+   * Search for one word in phrase. The search is not case
+   * sensitive. This method will check that the given goal
+   * is not a substring of a longer string (so, for
+   * example, "I know" does not contain "no"). The search
+   * begins at the beginning of the string.
+   * 
+   * @param statement
+   *            the string to search
+   * @param goal
+   *            the string to search for
+   * @return the index of the first occurrence of goal in
+   *         statement or -1 if it's not found
+   */
+  private int findKeyword(String statement, String goal)
   {
-    response = "Hmmm.";
+    return findKeyword(statement, goal, 0);
   }
-  else if (whichResponse == 2)
+  
+  /**
+   * Pick a default response to use if nothing else fits.
+   * @return a non-committal string
+   */
+  private String getRandomResponse ()
   {
-    response = "Do you really think so?";
+    Random r = new Random ();
+    return randomResponses [r.nextInt(randomResponses.length)];
   }
-  else if (whichResponse == 3)
-  {
-    response = "You don't say.";
-  }
-  else if (whichResponse == 4)
-  {
-    response = "Uh huh.";
-  }
-  else if (whichResponse == 5)
-  {
-    response = "I see.";
-  }
-  return response;
-}
+  
+  private String [] randomResponses = {
+    "Interesting, tell me more",
+    "Hmmm.",
+    "Do you really think so?",
+    "You don't say.",
+    "Uh huh.",
+    "I see.",
+  };
 }
